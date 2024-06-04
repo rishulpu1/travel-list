3f1b82ccd8bd2e9c3aefa43323b45b2e99e4c1f4
Rajneesh Kumar <rishulpu1@gmail.com>
2024-06-04 21:57:07 +0530

    Layout building

---
+++ src/components/PackingList.js
@@ -0,0 +1,9 @@
+function PackingList(){
+    return(
+        <div className="list">
+            List
+        </div>
+    );
+}
+
+export default PackingList;
---
+++ src/components/Stats.js
@@ -0,0 +1,9 @@
+function Stats(){
+    return(
+        <footer className="stats">
+            <em>You have X items on your list and you allready packed X (x%)</em>
+        </footer>
+    )
+}
+
+export default Stats;
